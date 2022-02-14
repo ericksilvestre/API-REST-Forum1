0b165141de54af5b37e68f9b75695b2673232fb7
Erick <erickaguiarsilvestre@gmail.com>
2022-02-14 15:15:17 -0300

    init

---
+++ projectForum/src/test/java/com/training/projectForum/ProjectForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.projectForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
